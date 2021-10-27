8528963e9504e3e3210fb2bd402bb1d9bb3dbf4e
toppingart123@gmail.com <56778279+toppingart@users.noreply.github.com>
2021-10-26 22:50:06 -0600

    testing pushing project and work

---
+++ project-part/app/src/main/java/com/example/all_habits/Comments.java
@@ -0,0 +1,8 @@
+package com.example.all_habits;
+
+public class Comments {
+    // id
+    // person who wrote it
+    // who the person wrote it for
+    // which habit the person wrote the comment on
+}
