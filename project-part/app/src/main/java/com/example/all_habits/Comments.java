7ef8ed8773a0f89d0ae2257ba06d320db64950b9
Hasei <58311797+hase-ki@users.noreply.github.com>
2021-11-03 16:28:24 -0600

    Reset Branch

--- project-part/app/src/main/java/com/example/all_habits/Comments.java
+++
@@ -1,8 +0,0 @@
-package com.example.all_habits;
-
-public class Comments {
-    // id
-    // person who wrote it
-    // who the person wrote it for
-    // which habit the person wrote the comment on
-}
--- wiki_images/README.md
+++
@@ -1,1 +0,0 @@
-This file contains images of the crc cards.
