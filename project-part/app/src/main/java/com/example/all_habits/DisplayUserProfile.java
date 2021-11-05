213d7ac3f2793500576a18400b382a3d5f4bf37a
Mahajabin Tokon <tokonislam1122@gmail.com>
2021-11-05 03:18:24 -0600

    Porfile updated

--- project-part/app/src/main/java/com/example/all_habits/DisplayUserProfile.java
+++ project-part/app/src/main/java/com/example/all_habits/DisplayUserProfile.java
@@ -6,13 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DisplayUserProfile extends AppCompatActivity {
+    TextView mlogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.displayuserprofile);
+
+        mlogout   = findViewById(R.id.logoutText);
+
+        mlogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Login.class));
+            }
+        });
+
+
     }
 
     public void onClick(View v) {
@@ -24,8 +37,5 @@ public class DisplayUserProfile extends AppCompatActivity {
     }
 
 
+
 }
-
-
-
-
