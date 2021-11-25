40c69a276fa43c2439ad643a97728993093a83b1
Hasei <58311797+hase-ki@users.noreply.github.com>
2021-11-25 14:46:52 -0700

    Pictures upload to firestore
    Not linked to habits yet

--- project-part/app/src/main/java/com/example/all_habits/cameraActivity.java
+++ project-part/app/src/main/java/com/example/all_habits/cameraActivity.java
@@ -5,6 +5,7 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -13,16 +14,25 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.util.UUID;
 
 public class cameraActivity extends AppCompatActivity {
 
@@ -30,20 +40,27 @@ public class cameraActivity extends AppCompatActivity {
     private StorageReference storage;
     ActivityResultLauncher<String> getContent;
     ActivityResultLauncher<Intent> activityResultLauncher;
+    public Uri imageUri;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
-
         Button takePhoto = findViewById(R.id.takePhotoButton);
         Button gallery = findViewById(R.id.galleryButton);
+        Button uploadButton = findViewById(R.id.uploadImageButton);
         ImageView habitPicture = findViewById(R.id.habitPicture);
         storage = FirebaseStorage.getInstance().getReference();
+
+
+        habitPicture.setDrawingCacheEnabled(true);
+        habitPicture.buildDrawingCache();
 
         getContent = registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
             @Override
             public void onActivityResult(Uri result) {
                 habitPicture.setImageURI(result);
+
             }
         });
 
@@ -57,6 +74,7 @@ public class cameraActivity extends AppCompatActivity {
                 }
             }
         });
+
 
 
         takePhoto.setOnClickListener(new View.OnClickListener() {
@@ -77,7 +95,32 @@ public class cameraActivity extends AppCompatActivity {
                 getContent.launch("image/*");
             }
         });
-    }
 
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(habitPicture.getDrawable() == null){
+                    Toast.makeText(getApplicationContext(), "There is no image to upload", Toast.LENGTH_SHORT).show();
+                }else {
+                    String randomName = UUID.randomUUID().toString();
+                    StorageReference storageRef = storage.child(randomName);
+                    // Create a reference to 'images/mountains.jpg'
+                    StorageReference storageImageRef = storage.child("images/" + randomName);
+
+                    // While the file names are the same, the references point to different files
+                    storageRef.getName().equals(storageImageRef.getName());    // true
+                    storageRef.getPath().equals(storageImageRef.getPath());
+
+                    Bitmap habitPictureBitMap = ((BitmapDrawable) habitPicture.getDrawable()).getBitmap();
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    habitPictureBitMap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                    byte[] data = baos.toByteArray();
+
+                    UploadTask uploadTask = storageRef.putBytes(data);
+                    finish();
+                }
+            }
+        });
+    }
 
 }
