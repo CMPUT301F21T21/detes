d8da1456fa891009504dd096ccc415f25d2cfcd9
Hasei <58311797+hase-ki@users.noreply.github.com>
2021-11-26 22:12:07 -0700

    Added CameraActivity and Camera functionality to habitEvents

--- project-part/app/src/main/java/com/example/all_habits/HabitEvents.java
+++ project-part/app/src/main/java/com/example/all_habits/HabitEvents.java
@@ -1,58 +1,96 @@
 package com.example.all_habits;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class HabitEvents extends AppCompatActivity {
 
+    final long ONE_MEGABYTE = 1024 * 1024;
     private TextView commentEditText;
+    private ImageView habitEventImage;
     private Button saveCommentButton;
-
-    private int habitNum;
+    private Button habitEventImageButton;
+    public String photoName;
     private String habitId;
     private String commentString;
     private ArrayAdapter<Comment> commentAdapter;
-
     FirebaseFirestore db;
     private FirebaseUser currentFireBaseUser;
     private CollectionReference habitReference; // collection of selected habit
     private CollectionReference userCollectionReference; // collection of signed in user
     private DocumentReference documentRef;
+    private StorageReference storageRef;
+    private StorageReference imageRef;
+    private StorageReference resetRef;
+    private int habitNum;
 
+    //When activity is returned to. checks if the habit picture has been changed.
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        db.collection(currentFireBaseUser.getUid()).document(habitId).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if(task.isSuccessful()){
+                    photoName = task.getResult().getString("optionalPhoto");
+                    if(photoName != null) {
+                        storageRef = FirebaseStorage.getInstance().getReferenceFromUrl("gs://projecthabits.appspot.com");
+                        resetRef = storageRef.child(photoName);
+                        resetRef.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                            @Override
+                            public void onSuccess(byte[] bytes) {
+                                Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                                habitEventImage.setImageBitmap(bmp);
+                            }
+                        });
+                    }
+                }
+            }
+        });
+
+    }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.habit_events);
-
         Intent intent = getIntent();
         habitNum = intent.getIntExtra("habitNum", 1); // gets the habit that was clicked on
 
         db = FirebaseFirestore.getInstance();
         currentFireBaseUser = FirebaseAuth.getInstance().getCurrentUser();
         CollectionReference collectionReference = db.collection(currentFireBaseUser.getUid().toString());
+        storageRef = FirebaseStorage.getInstance().getReferenceFromUrl("gs://projecthabits.appspot.com");
         //userCollectionReference = db.collection(currentFireBaseUser.getUid());
-
         commentEditText = findViewById(R.id.commentEditText);
         saveCommentButton = findViewById(R.id.saveCommentButton);
+        habitEventImageButton = findViewById(R.id.habitEventImageButton);
+        habitEventImage = findViewById(R.id.habitEventImage);
 
         Query findHabit = db.collection(currentFireBaseUser.getUid()).whereEqualTo("habitNum", habitNum).limit(1);
         findHabit.get()
@@ -64,7 +102,7 @@ public class HabitEvents extends AppCompatActivity {
                                 habitId = document.getId();
                                 documentRef = db.collection(currentFireBaseUser.getUid()).document(habitId);
                                 commentString = document.getString("comment");
-
+                                photoName = document.getString("optionalPhoto");
                                 // if the comment string is not empty
                                 if (!commentString.equals("")) {
                                     commentEditText.setText(document.getString("comment"));
@@ -74,22 +112,45 @@ public class HabitEvents extends AppCompatActivity {
                             }
                         }
 
-        saveCommentButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                        saveCommentButton.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
 
-                if (commentEditText.getText().toString().length() > 20){
-                    Toast.makeText(getApplicationContext(), "The comment has to be under 20 characters long.", Toast.LENGTH_SHORT).show();
-                    return;
-                }
+                                if (commentEditText.getText().toString().length() > 20) {
+                                    Toast.makeText(getApplicationContext(), "The comment has to be under 20 characters long.", Toast.LENGTH_SHORT).show();
+                                    return;
+                                }
 
-                documentRef.update("comment", commentEditText.getText().toString());
-                Toast.makeText(getApplicationContext(), "Your comment has been saved", Toast.LENGTH_SHORT).show();
+                                documentRef.update("comment", commentEditText.getText().toString());
+                                Toast.makeText(getApplicationContext(), "Your comment has been saved", Toast.LENGTH_SHORT).show();
 
 
-              }});
-            }
+                            }
+                        });
+
+                        //Starts CameraActivity.
+                        habitEventImageButton.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Intent intent = new Intent(getApplicationContext(), cameraActivity.class);
+                                intent.putExtra("habitId", habitId);
+                                intent.putExtra("photoName", photoName);
+                                startActivity(intent);
+                            }
+                        });
+
+                        //If this habit has a picture, display it in the habitEventImage.
+                        if (photoName != null) {
+                            imageRef = storageRef.child(photoName);
+                            imageRef.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                                @Override
+                                public void onSuccess(byte[] bytes) {
+                                    Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                                    habitEventImage.setImageBitmap(bmp);
+                                }
+                            });
+                        }
+                    }
         });
-
     }
 }
--- project-part/app/src/main/java/com/example/all_habits/cameraActivity.java
+++ project-part/app/src/main/java/com/example/all_habits/cameraActivity.java
@@ -4,9 +4,11 @@ import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
@@ -15,6 +17,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
@@ -29,6 +34,7 @@ public class cameraActivity extends AppCompatActivity {
 
 
     private StorageReference storage;
+    private StorageReference imageRef;
     ActivityResultLauncher<String> getContent;
     ActivityResultLauncher<Intent> activityResultLauncher;
 
@@ -37,22 +43,42 @@ public class cameraActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        Intent intent = getIntent();
+
         Button takePhoto = findViewById(R.id.takePhotoButton);
         Button gallery = findViewById(R.id.galleryButton);
         Button uploadButton = findViewById(R.id.uploadImageButton);
         ImageView habitPicture = findViewById(R.id.habitPicture);
+        String habitId = intent.getStringExtra("habitId");
+        String photoName = intent.getStringExtra("photoName");
         storage = FirebaseStorage.getInstance().getReference();
 
+        //Allows the habit picture to be downloaded to the firestore storage.
         habitPicture.setDrawingCacheEnabled(true);
         habitPicture.buildDrawingCache();
         FirebaseFirestore db;
         FirebaseUser currentFireBaseUser;
+        StorageReference storageRefUrl = FirebaseStorage.getInstance().getReferenceFromUrl("gs://projecthabits.appspot.com");
 
         // create an instance of the firestore
         db = FirebaseFirestore.getInstance();
         currentFireBaseUser = FirebaseAuth.getInstance().getCurrentUser();
         final CollectionReference collectionReference = db.collection(currentFireBaseUser.getUid().toString());
 
+
+        if(photoName != null){
+            imageRef = storageRefUrl.child(photoName);
+            final long ONE_MEGABYTE = 1024 * 1024;
+            imageRef.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                @Override
+                public void onSuccess(byte[] bytes) {
+                    Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                    habitPicture.setImageBitmap(bmp);
+
+                }
+            });
+        }
+        //Sets the imageView to the gallery chosen picture.
         getContent = registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
             @Override
             public void onActivityResult(Uri result) {
@@ -61,6 +87,7 @@ public class cameraActivity extends AppCompatActivity {
             }
         });
 
+        //Sets the imageView to the camera picture
         activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
             @Override
             public void onActivityResult(ActivityResult result) {
@@ -72,6 +99,7 @@ public class cameraActivity extends AppCompatActivity {
             }
         });
 
+        //Opens the camera application.
         takePhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -84,6 +112,7 @@ public class cameraActivity extends AppCompatActivity {
             }
         });
 
+        //opens the gallery application.
         gallery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -91,28 +120,53 @@ public class cameraActivity extends AppCompatActivity {
             }
         });
 
+        //Uploads the imageView picture to the firestore db.
         uploadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                //If the imageView is empty, don't upload any image.
                 if(habitPicture.getDrawable() == null){
                     Toast.makeText(getApplicationContext(), "There is no image to upload", Toast.LENGTH_SHORT).show();
                 }else {
+
+                    if(photoName != null){
+                        imageRef.delete();
+                    }
                     String randomName = UUID.randomUUID().toString();
-                    StorageReference storageRef = storage.child(randomName);
+                    StorageReference uploadRef = storage.child(randomName);
                     // Create a reference to 'images/mountains.jpg'
                     StorageReference storageImageRef = storage.child("images/" + randomName);
 
                     // While the file names are the same, the references point to different files
-                    storageRef.getName().equals(storageImageRef.getName());    // true
-                    storageRef.getPath().equals(storageImageRef.getPath());
+                    uploadRef.getName().equals(storageImageRef.getName());    // true
+                    uploadRef.getPath().equals(storageImageRef.getPath());
 
+                    //Converts imageView into bytes to send into firestore storage.
                     Bitmap habitPictureBitMap = ((BitmapDrawable) habitPicture.getDrawable()).getBitmap();
                     ByteArrayOutputStream baos = new ByteArrayOutputStream();
                     habitPictureBitMap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
                     byte[] data = baos.toByteArray();
+                    db.collection(currentFireBaseUser.getUid()).document(habitId).update("optionalPhoto",randomName);
+                    UploadTask uploadTask = uploadRef.putBytes(data);
+                    Toast.makeText(getApplicationContext(), "Uploading...", Toast.LENGTH_SHORT).show();
 
-                    UploadTask uploadTask = storageRef.putBytes(data);
-                    finish();
+                    //Closes activity after file is finished uploading.
+                    uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Toast.makeText(getApplicationContext(), "Done~", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                    });
+
+                    //Sends a message if file fails to upload.
+                    uploadTask.addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getApplicationContext(), "Failed to upload", Toast.LENGTH_SHORT).show();
+                        }
+                    });
                 }
             }
         });
