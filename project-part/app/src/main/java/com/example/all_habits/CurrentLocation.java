7da2b7dd1ff949bf56cd6270b97c298dc4db5a8a
Diana Le <dble@ualberta.ca>
2021-11-27 21:54:03 -0700

    drag

--- project-part/app/src/main/java/com/example/all_habits/CurrentLocation.java
+++ project-part/app/src/main/java/com/example/all_habits/CurrentLocation.java
@@ -9,6 +9,8 @@ import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -18,19 +20,20 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
 
 /**
  * Get the optional location for where the habit was completed
  */
-public class CurrentLocation extends AppCompatActivity {
+public class CurrentLocation extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMarkerDragListener {
+
+    private GoogleMap mMap;
 
     FusedLocationProviderClient client;
     SupportMapFragment MapFragment;
@@ -38,7 +41,6 @@ public class CurrentLocation extends AppCompatActivity {
     private MarkerOptions markerOptions;
 
     Button uploadButton;
-    private StorageReference storage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,47 +50,66 @@ public class CurrentLocation extends AppCompatActivity {
         client = LocationServices.getFusedLocationProviderClient(this);
         MapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.google_map);
 
+        assert MapFragment != null;
+        MapFragment.getMapAsync(this);
+
         // check that we have permission to access location
         if (ActivityCompat.checkSelfPermission(CurrentLocation.this,
-                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             // if we do, get the current location
-            @SuppressLint("MissingPermission")
-            Task<Location> task = client.getLastLocation();
-            task.addOnSuccessListener(new OnSuccessListener<Location>() {
-                @Override
-                public void onSuccess(Location location) {
-                    if (location != null) {
-                        MapFragment.getMapAsync(new OnMapReadyCallback() {
-                            @Override
-                            public void onMapReady(GoogleMap googleMap) {
-                                latLng = new LatLng(location.getLatitude(), location.getLongitude());
-                                markerOptions = new MarkerOptions().position(latLng).title("Current location");
-                                googleMap.addMarker(markerOptions);
-                                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
-                            }
-                        });
-                        // upload the current location to firebase
-                        Intent intent = getIntent();
-                        String habitId = intent.getStringExtra("habitId");
-                        uploadButton = findViewById(R.id.uploadLocationButton);
-                        storage = FirebaseStorage.getInstance().getReference();
-                        FirebaseFirestore db = FirebaseFirestore.getInstance();
-                        FirebaseUser currentFireBaseUser = FirebaseAuth.getInstance().getCurrentUser();
-
-                        uploadButton.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                locationCoordinate coordinate = new locationCoordinate(location.getLatitude(),location.getLongitude());
-                                db.collection(currentFireBaseUser.getUid()).document(habitId).update("optionalLocation",coordinate);
-                            }
-                        });
-                    }
-                }
-            });
-        }else{
+            onMapReady(mMap);
+            mMap.setOnMarkerDragListener(this);
+        } else {
             ActivityCompat.requestPermissions(CurrentLocation.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
         }
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        @SuppressLint("MissingPermission")
+        Task<Location> task = client.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                    markerOptions = new MarkerOptions().position(latLng).title("Current location");
+                    markerOptions = new MarkerOptions().draggable(true);
+                    mMap.addMarker(markerOptions);
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public void onMarkerDragEnd(@NonNull Marker marker) {
+        latLng = new LatLng(marker.getPosition().latitude, marker.getPosition().longitude);
+
+        // upload the current location to firebase
+        Intent intent = getIntent();
+        String habitId = intent.getStringExtra("habitId");
+        uploadButton = findViewById(R.id.uploadLocationButton);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseUser currentFireBaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //locationCoordinate coordinate = new locationCoordinate(location.getLatitude(), location.getLongitude());
+                db.collection(currentFireBaseUser.getUid()).document(habitId).update("optionalLocation", latLng);
+            }
+        });
+    }
+
+    @Override
+    public void onMarkerDrag (@NonNull Marker marker){
+    }
+
+    @Override
+    public void onMarkerDragStart (@NonNull Marker marker){
     }
 }
-
-
