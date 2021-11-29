5be3308e8ae5c606e1d79a3adeab68f9bc670067
toppingart123@gmail.com <56778279+toppingart@users.noreply.github.com>
2021-11-29 00:34:37 -0700

    today's list and added to Create

--- project-part/app/src/main/java/com/example/all_habits/TodaysHabits.java
+++ project-part/app/src/main/java/com/example/all_habits/TodaysHabits.java
@@ -1,20 +1,28 @@
 package com.example.all_habits;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import java.text.ParseException;
@@ -37,13 +45,18 @@ public class TodaysHabits extends AppCompatActivity {
     ListView todayListView;
     ArrayList<Habit> todayArrayList;
     ArrayAdapter<Habit> todayAdapter;
+    String currentDateString;
+    ArrayList<Integer> completedHabitsList; // tracked using habit nums
+
+    ListView completedListView;
+    ArrayList<Habit> completedArrayList;
+    ArrayAdapter<Habit> completedAdapter;
+
     String[] daysOfTheWeek = {"Sun","Mon","Tues","Wed","Thurs","Fri","Sat"};
 
     //firestore attribute
     FirebaseFirestore db;
     private FirebaseUser currentFireBaseUser;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,8 +77,13 @@ public class TodaysHabits extends AppCompatActivity {
         todayListView = findViewById(R.id.todays_list_pending);
         todayArrayList = new ArrayList<>();
 
-        todayAdapter = new HabitsList(this, todayArrayList);
+        todayAdapter = new TodaysList(this, todayArrayList, true);
         todayListView.setAdapter(todayAdapter); //converts data source to ListView
+
+        completedListView = findViewById(R.id.todays_list_completed);
+        completedArrayList = new ArrayList<>();
+        completedAdapter = new TodaysList(this, completedArrayList, false);
+        completedListView.setAdapter(completedAdapter); //converts data source to ListView
 
 
         // getting data from firebase to your local device (snapshot of database)
@@ -74,6 +92,9 @@ public class TodaysHabits extends AppCompatActivity {
             public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable
                     FirebaseFirestoreException error) {
                 todayArrayList.clear();
+                completedArrayList.clear();
+                ArrayList<Integer> trackList = new ArrayList<Integer>();
+
                 ArrayList<String> habitDays;
                 String dateString;
                 for(QueryDocumentSnapshot habits: queryDocumentSnapshots)
@@ -84,7 +105,7 @@ public class TodaysHabits extends AppCompatActivity {
 
                     //Formats the string into the form 01/01/1990
                     SimpleDateFormat simpleFormat = new SimpleDateFormat("dd/MM/yyyy");
-                    String currentDateString = simpleFormat.format(c.getTime());
+                    currentDateString = simpleFormat.format(c.getTime());
                     Date date1 = null;
 
                     c.set(Calendar.MILLISECOND, 0);
@@ -100,17 +121,72 @@ public class TodaysHabits extends AppCompatActivity {
                     if(habit.getHabitName() != null) {
                         for(int i = 0;i < habitDays.size();i++){
                             if(habitDays.get(i).toString().equals(day)){
-                                if(date1.before(date2) || (date1.compareTo(date2) == 0))
+                                if(date1.before(date2) || (date1.compareTo(date2) == 0) && !trackList.contains(habit.getHabitNum()))
                                 todayArrayList.add(habit);
+                                trackList.add(habit.getHabitNum());
                             }
                             Log.d("Tag", date1.toString());
                         }
                     }
+
+                    completedHabitsList = new ArrayList<Integer>();
+                    if (habit.getCompletedDaysList().contains(currentDateString) && !completedHabitsList.contains(habit.getHabitNum())){
+                        completedHabitsList.add(habit.getHabitNum());
+                        todayArrayList.remove(habit);
+                        completedArrayList.add(habit);
+                    }
                 }
                 todayAdapter.notifyDataSetChanged(); // Notifying the adapter to render any new data fetched
                 //from the cloud
+                completedAdapter.notifyDataSetChanged();
+
+                Log.d("todayArrayList_", String.valueOf(todayArrayList));
+                Log.d("completedArrayList_", String.valueOf(completedArrayList));
             }
         });
+
+        todayListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Habit habit = todayArrayList.get(position);
+
+                if (!completedHabitsList.contains(habit.getHabitNum())) {
+                    completedArrayList.add(todayArrayList.get(position)); // adding today's habit that was completed
+                }
+                todayArrayList.remove(todayArrayList.get(position));
+
+                if (!(habit.getCompletedDaysList().contains(currentDateString))) {
+                    habit.addToCompletedDaysList(currentDateString);
+                }
+                habit.setProgress();
+
+                Query findHabit = db.collection(currentFireBaseUser.getUid()).whereEqualTo("habitNum", habit.getHabitNum()).limit(1);
+                findHabit.get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                        String habitId = document.getId();
+                                        DocumentReference documentRef = db.collection(currentFireBaseUser.getUid()).document(habitId);
+                                        documentRef.update("completedDaysList", habit.getCompletedDaysList());
+                                        documentRef.update("progress", habit.getProgress());
+                                    }
+
+                                }
+
+                            }
+                        });
+
+                todayAdapter.notifyDataSetChanged(); // Notifying the adapter to render any new data fetched
+                //from the cloud
+                completedAdapter.notifyDataSetChanged();
+            }
+
+        });
+
+        completedAdapter.notifyDataSetChanged();
+        todayAdapter.notifyDataSetChanged();
 
         habitButton = findViewById(R.id.allHabits);
         habitButton.setOnClickListener(new View.OnClickListener() {
@@ -128,6 +204,8 @@ public class TodaysHabits extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(TodaysHabits.this, Create.class);
                 startActivity(intent);
+                todayAdapter.notifyDataSetChanged();
+                completedAdapter.notifyDataSetChanged();
             }
         });
 
